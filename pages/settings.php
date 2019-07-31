9d7acdb2dd08f2bfd190f9020251a6465b63de4e
Thomas Skerbis <thomas.skerbis@mac.com>
2019-07-31 18:22:25 +0200

    Get Javascript-Tag

--- pages/settings.php
+++ pages/settings.php
@@ -24,3 +24,42 @@ $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', "Matomo Settings", false);
 $fragment->setVar('body', $form->get(), false);
 echo $fragment->parse('core/page/section.php');
+
+
+
+if (rex::isBackend() 
+    and $addon->getConfig('token')!='' 
+    and $addon->getConfig('user')!='' 
+    and $addon->getConfig('password')!=''
+    and $addon->getConfig('url')!='' 
+    and $addon->getConfig('id')!=''
+   )
+{
+
+
+$url = 'https://klxm.de/piwik//index.php?module=API&method=SitesManager.getJavascriptTag&idSite=7&piwikUrl=&format=JSON&token_auth='.$addon->getConfig('token');
+
+?><?php  
+//URL of targeted site  
+$ch = curl_init();  
+
+// set URL and other appropriate options  
+curl_setopt($ch, CURLOPT_URL, $url);  
+curl_setopt($ch, CURLOPT_HEADER, 0);  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+
+// grab URL and pass it to the browser  
+
+#$output = curl_exec($ch);  
+
+// close curl resource, and free up system resources  
+$output = json_decode(curl_exec($ch),true);
+
+
+
+#echo json_decode($output);
+echo '<div class="rex-form-group form-group"><textarea class="form-control" height="80">'.$output['value'].'</textarea></div>';
+}
+?>  
+
+
