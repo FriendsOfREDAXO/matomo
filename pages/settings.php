a45866a5883d6f60dfd8f8cb9204ce42c365f7b5
Thomas Skerbis <thomas.skerbis@mac.com>
2019-08-01 14:51:59 +0200

    Update settings.php

--- pages/settings.php
+++ pages/settings.php
@@ -1,73 +1,71 @@
-<?php 
+<?php
 $addon = rex_addon::get('matomo');
 $form = rex_config_form::factory($addon->name);
 
-$field = $form->addInputField('text', 'url',null,["class" => "form-control"]);
+$field = $form->addInputField('text', 'url', null, ["class" => "form-control"]);
 $field->setLabel($addon->i18n('matomo_url'));
 
-$field = $form->addInputField('text', 'id',null,["class" => "form-control"]);
+$field = $form->addInputField('text', 'id', null, ["class" => "form-control"]);
 $field->setLabel($addon->i18n('matomo_id'));
 
-
-$field = $form->addInputField('text', 'token',null,["class" => "form-control"]);
+$field = $form->addInputField('text', 'token', null, ["class" => "form-control"]);
 $field->setLabel($addon->i18n('matomo_token'));
 
-$field = $form->addInputField('text', 'user',null,["class" => "form-control"]);
+$field = $form->addInputField('text', 'user', null, ["class" => "form-control"]);
 $field->setLabel($addon->i18n('matomo_user'));
 
-$field = $form->addInputField('password', 'password',null,["class" => "form-control"]);
+$field = $form->addInputField('password', 'password', null, ["class" => "form-control"]);
 $field->setLabel($addon->i18n('matomo_password'));
-
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', "Matomo Settings", false);
-$fragment->setVar('body', $form->get(), false);
+$fragment->setVar('body', $form->get() , false);
 echo $fragment->parse('core/page/section.php');
 
-
-
-if (rex::isBackend() 
-    and $addon->getConfig('token')!='' 
-    and $addon->getConfig('user')!='' 
-    and $addon->getConfig('password')!=''
-    and $addon->getConfig('url')!='' 
-    and $addon->getConfig('id')!=''
-   )
+if (rex::isBackend() and $addon->getConfig('token') != '' and $addon->getConfig('user') != '' and $addon->getConfig('password') != '' and $addon->getConfig('url') != '' and $addon->getConfig('id') != '')
 {
 
+    $url = rex_escape($addon->getConfig('url')) . 'index.php?module=API&method=SitesManager.getJavascriptTag&idSite=' . rex_escape($addon->getConfig('id')) . '&format=JSON&token_auth=' . $addon->getConfig('token');
 
-$url = rex_escape($addon->getConfig('url')).'index.php?module=API&method=SitesManager.getJavascriptTag&idSite='.rex_escape($addon->getConfig('id')).'&format=JSON&token_auth='.$addon->getConfig('token');
+?>
 
-?><?php  
-//URL of targeted site  
-$ch = curl_init();  
+<?php
+    //URL of targeted site
+    $ch = curl_init();
 
-// set URL and other appropriate options  
-curl_setopt($ch, CURLOPT_URL, $url);  
-curl_setopt($ch, CURLOPT_HEADER, 0);  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+    // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// close curl resource, and free up system resources  
-$output = json_decode(curl_exec($ch),true);
+    // close curl resource, and free up system resources
+    $output = json_decode(curl_exec($ch) , true);
+    if(array_key_exists("value",$output))
+    {
+        $check = strpos($output['value'], 'Matomo');
+    }
+    else
+    {
+        $check = false;
+    }
 
-$check = strpos($output['value'] , 'Matomo');
+    if ($check !== false)
+    {
 
-if ($check  !== false) {
+        $content = '<p>' . $addon->i18n('matomo_trackingcode') . '</p><div class="rex-form-group form-group"><textarea class="form-control codemirror" height="80">' . $output['value'] . '</textarea></div>';
+        $addon->setConfig('matomocheck', true);
+        $addon->setConfig('matomojs', $output['value']);
 
-$content =  '<p>'.$addon->i18n('matomo_trackingcode').'</p><div class="rex-form-group form-group"><textarea class="form-control codemirror" height="80">'.$output['value'].'</textarea></div>';
-     $addon->setConfig('matomocheck', true);
-     $addon->setConfig('matomojs', $output['value']);
-    
-    $fragment = new rex_fragment();
-$fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', $addon->i18n('matomo_trackingcode_headline'), false);
-$fragment->setVar('body', $content, false);
-echo $fragment->parse('core/page/section.php');
-
+        $fragment = new rex_fragment();
+        $fragment->setVar('class', 'edit', false);
+        $fragment->setVar('title', $addon->i18n('matomo_trackingcode_headline') , false);
+        $fragment->setVar('body', $content, false);
+        echo $fragment->parse('core/page/section.php');
 
     }
-    else {
+    else
+    {
         $content = '<div class="alert alert-danger">';
         $content .= $addon->i18n('matomo_notfound');
         $content .= '</div>';
@@ -76,5 +74,3 @@ echo $fragment->parse('core/page/section.php');
     }
 
 }
-
-
